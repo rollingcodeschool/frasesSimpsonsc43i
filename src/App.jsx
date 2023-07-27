6d529c3a9a60f5b2fc5e1a233d69d4ca50075748
Emilse Arias <emilsearias@gmail.com>
2023-07-26 21:10:14 -0300

    mostrar el spinner cuando seleccionamos obtener frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 function App() {
   const [personaje, setPersonaje] = useState({});
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
 
   // useEffect solo en montaje
   useEffect(() => {
@@ -16,14 +17,18 @@ function App() {
 
   const consultarApi = async () => {
     try {
+      setMostrarSpinner(true);
       //codigo que quiero ejecutar
-      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      );
       const dato = await respuesta.json();
-      console.log(respuesta)
-      console.log(dato[0])
+      console.log(respuesta);
+      console.log(dato[0]);
       // guardar la respuesta en el state
-      setPersonaje(dato[0])
-
+      setPersonaje(dato[0]);
+      //ocultar el spinner
+      setMostrarSpinner(false);
     } catch (error) {
       console.log(error);
       // dejar un mensaje intuitivo para el usuario final
@@ -34,8 +39,16 @@ function App() {
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase datosPersonaje={personaje}></Frase>
-        <Button variant="warning" onClick={consultarApi}>Obtener frase</Button>
+        {mostrarSpinner === true ? (
+          <div className="my-5">
+            <Spinner animation="border" variant="light" />
+          </div>
+        ) : (
+          <Frase datosPersonaje={personaje}></Frase>
+        )}
+        <Button variant="warning" onClick={consultarApi}>
+          Obtener frase
+        </Button>
       </Container>
     </>
   );
